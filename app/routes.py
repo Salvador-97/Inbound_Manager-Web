0cf2646d9e34751e92045b7a06a804b9940b8872
Salvador-97 <salvador.go_97@hotmail.com>
2025-08-10 22:28:58 -0600

    division-paginas

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('home.html')
+
+@main.route('/contenedores')
+def contenedores():
+    return render_template('contenedores.html')
+
+@main.route('/configuracion')
+def configuracion():
+    return render_template('configuracion.html')
