690bb13e47a7116a3ea35835ea3a50b5aa94f20e
Salvador-97 <salvador.go_97@hotmail.com>
2025-08-12 22:42:00 -0600

    conexion-db

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from sqlalchemy import create_engine, text
+
+baseDatos = create_engine(r'sqlite:///C:\Users\salva\Documents\Inbound_Web\Inbound_Manager-Web\app\static\db\productos.db')
 
 def crearApp():
     app = Flask(__name__)
@@ -6,4 +9,11 @@ def crearApp():
     
     app.register_blueprint(main)
     
+    with baseDatos.connect() as connection:
+        listaInformacion = []
+        resultado = connection.execute(text('SELECT * FROM productos WHERE SKU="BB25C22"'))
+        for row in resultado:
+            print(row)
+            listaInformacion.append(row)
+        print("Descripcion: ", listaInformacion[0])
     return app
