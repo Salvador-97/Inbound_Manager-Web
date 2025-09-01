53684923f131f36992433ea7720993bf9f5413a4
Salvador-97 <salvador.go_97@hotmail.com>
2025-09-01 15:30:57 -0600

    mostrar-producto

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,10 @@
 from flask import Flask
 from sqlalchemy import create_engine, text
 
-baseDatos = create_engine(r'sqlite:///C:\Users\salva\Documents\Inbound_Web\Inbound_Manager-Web\app\static\db\productos.db')
-
 def crearApp():
     app = Flask(__name__)
     from .routes import main
     
     app.register_blueprint(main)
     
-    with baseDatos.connect() as connection:
-        listaInformacion = []
-        resultado = connection.execute(text('SELECT * FROM productos WHERE SKU="BB25C22"'))
-        for row in resultado:
-            print(row)
-            listaInformacion.append(row)
-        print("Descripcion: ", listaInformacion[0])
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from sqlalchemy import create_engine, text 
 
 main = Blueprint('main', __name__, template_folder='app/templates')
+baseDatos = create_engine(r'sqlite:///app/static\db\productos.db')
 
 @main.route('/')
 def home():
     return render_template('home.html')
 
-@main.route('/contenedores')
+@main.route('/contenedores', methods=['GET', 'POST'])
 def contenedores():
-    return render_template('contenedores.html')
+    prod = None
+    if request.method == 'GET':
+        producto = request.args.get('sku_producto')
+        with baseDatos.connect() as connection:
+            consulta = text('SELECT * FROM productos WHERE SKU = :sku_producto')
+            resultado = connection.execute(consulta, {"sku_producto": producto})
+            prod = resultado.fetchone()
+    return render_template('contenedores.html', producto=prod)
 
 @main.route('/configuracion')
 def configuracion():
     return render_template('configuracion.html')
+
+@main.route('/productos')
+def productos():
+    return render_template('productos.html')
